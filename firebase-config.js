aeadbe4d9b320a386d091cb54dcd54c7443b54ed
胡珈琪 <hujiaqi@kos-MacBook-Air.local>
2026-02-26 12:07:49 -0800

    Add Firebase project config values

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 window.WISHLIST_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCBL93AnbE8k6Nhsl82Jsugy7Stxu3S3CE",
+  authDomain: "wishlist-b3f04.firebaseapp.com",
+  projectId: "wishlist-b3f04",
+  storageBucket: "wishlist-b3f04.firebasestorage.app",
+  messagingSenderId: "924131760654",
+  appId: "1:924131760654:web:013acc47a14b0d3a63ba5f",
+  measurementId: "G-5WY8Z96W3N"
 };
